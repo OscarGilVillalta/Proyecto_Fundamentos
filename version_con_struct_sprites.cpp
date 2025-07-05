244392ad794a4e32815de9ec0bc41c650756b942
Rodrigo Alejandro Campos Cierra <00167625@uca.edu.sv>
2025-07-05 16:22:58 -0600

    Update and rename load_file.cpp to version_con_struct_sprites.cpp

--- version_con_struct_sprites.cpp
+++ version_con_struct_sprites.cpp
@@ -12,20 +12,56 @@ struct Difficulty
     int maxRows;    // Número máximo de filas
     int maxColumns; // Número máximo de columnas
     int maxBombs;   // Número máximo de bombas
-
+    int maxBullets; // Número máximo de balas
+    int maxShields; // Número máximo de escudos
     void reset()
     { // Restablece los valores predeterminados
         maxRows = 0;
         maxColumns = 0;
         maxBombs = 0;
+        maxBullets = 0;
+        maxShields = 0;
     }
 } difficulty;
 
+struct Sprites {
+    string title =
+        " ___                                                                      ___ \n"
+        "( _ )--------------------------------------------------------------------( _ )\n"
+        " | |                                                                      | | \n"
+        " | |                                                                      | | \n"
+        " | |   ######  ######  ##  ##  #####           ######  ##  ##  ######     | | \n"
+        " | |   ##        ##    ### ##  ##  ##            ##    ##  ##  ##         | | \n"
+        " | |   ####      ##    ## ###  ##  ##            ##    ######  ####       | | \n"
+        " | |   ##        ##    ##  ##  ##  ##            ##    ##  ##  ##         | | \n"
+        " | |   ##      ######  ##  ##  #####             ##    ##  ##  ######     | | \n"
+        " | |   ................................................................   | | \n"
+        " | |   ######  #####   ######   ####    ####    ##  ##  #####   ######    | | \n"
+        " | |     ##    ##  ##  ##      ##  ##  ##       ##  ##  ##  ##  ##        | | \n"
+        " | |     ##    #####   ####    ######   ####    ##  ##  #####   ####      | | \n"
+        " | |     ##    ##  ##  ##      ##  ##      ##   ##  ##  ##  ##  ##        | | \n"
+        " | |     ##    ##  ##  ######  ##  ##   ####     ####   ##  ##  ######    | | \n"
+        " | |   ................................................................   | | \n"
+        " | |                                                                      | | \n"
+        " |_|                                                                      |_| \n"
+        "(___)--------------------------------------------------------------------(___)\n";
+
+    string menu =
+        "|------------------------------------------|\n"
+        "|   **** ENCUENTRA EL TESORO ****          |\n"
+        "|__________________________________________|\n"
+        "|  1. Jugar modo Solitario                 |\n"
+        "|  2. Jugar modo Multijugador              |\n"
+        "|  3. Cargar partida                       |\n"
+        "|  4. Guardar partida                      |\n"
+        "|  5. Salir del juego                      |\n"
+        "|------------------------------------------|\n"
+        "Seleccione una opción: ";
+} sprites;
 struct GameData
 {
     int max_players;
     bool game_status = true;
-    vector<vector<int>> bombXY = random_coordinates();
     vector<vector<int>> bomb_explote; // Coordenadas de bombas explotadas
     vector<vector<int>> repeat;       // Coordenadas repetidas
     vector<vector<int>> treasureXY;   // Coordenadas de tesoros encontrados
@@ -43,8 +79,10 @@ struct Players
 {
     string name;   // Nombre del jugador
     int points;    // Puntos acumulados
+    int bullets;   // Balas disponibles
+    int shields;   // Escudo disponible
     bool is_alive; // Indica si el jugador está vivo
-} players[10];
+} players[5];
 
 // Estructura para manejar el estado del juego
 struct ErrorType
@@ -70,47 +108,22 @@ void menu_difficulty(int dif);
 vector<vector<int>> random_coordinates();
 bool prove_coordinates(const vector<int> &coordinate, const vector<vector<int>> &bombXY);
 void game_over_message();
-string sprite(string typeSprite);
 bool victory(int points);
 void save_game(const vector<vector<int>> &bombXY);
 void load_game(vector<vector<int>> &bombXY);
-int game_multiplayer();
-void reset_game_state();
+void game_multiplayer(vector<vector<int>> &bombXY);
+void reset_game_state(vector<vector<int>>& bombXY);
+void pistol_shot(int &turn, bool &lose);
+void shield_protection(int &turn, bool &lose);
+void player_action(int &turn, bool &lose);
+void player_configuration();
 
 int main()
 {
     srand(time(NULL));
-    sprite("Title");
-    menu_difficulty(2);
+    cout<< Sprites.title;
+    game_menu();
     return 0;
-}
-
-// Función para mostrar gráficos ASCII del juego
-string sprite(string typeSprite)
-{
-    if (typeSprite == "Title")
-    {
-        cout << " ___                                                                      ___ \n"
-                "( _ )--------------------------------------------------------------------( _ )\n"
-                " | |                                                                      | | \n"
-                " | |                                                                      | | \n"
-                " | |   ######  ######  ##  ##  #####           ######  ##  ##  ######     | | \n"
-                " | |   ##        ##    ### ##  ##  ##            ##    ##  ##  ##         | | \n"
-                " | |   ####      ##    ## ###  ##  ##            ##    ######  ####       | | \n"
-                " | |   ##        ##    ##  ##  ##  ##            ##    ##  ##  ##         | | \n"
-                " | |   ##      ######  ##  ##  #####             ##    ##  ##  ######     | | \n"
-                " | |   ................................................................   | | \n"
-                " | |   ######  #####   ######   ####    ####    ##  ##  #####   ######    | | \n"
-                " | |     ##    ##  ##  ##      ##  ##  ##       ##  ##  ##  ##  ##        | | \n"
-                " | |     ##    #####   ####    ######   ####    ##  ##  #####   ####      | | \n"
-                " | |     ##    ##  ##  ##      ##  ##      ##   ##  ##  ##  ##  ##        | | \n"
-                " | |     ##    ##  ##  ######  ##  ##   ####     ####   ##  ##  ######    | | \n"
-                " | |   ................................................................   | | \n"
-                " | |                                                                      | | \n"
-                " |_|                                                                      |_| \n"
-                "(___)--------------------------------------------------------------------(___)\n";
-    }
-    return "";
 }
 
 // Función para configurar la dificultad del juego
@@ -122,21 +135,26 @@ void menu_difficulty(int dif)
         difficulty.maxRows = 10;
         difficulty.maxColumns = 10;
         difficulty.maxBombs = 30;
+        difficulty.maxBullets = 10;
+        difficulty.maxShields = 5;
         break;
     case 2: // Medio
         difficulty.maxRows = 20;
         difficulty.maxColumns = 20;
         difficulty.maxBombs = 80;
+        difficulty.maxBullets = 10;
+        difficulty.maxShields = 5;
         break;
     case 3: // Difícil
         difficulty.maxRows = 30;
         difficulty.maxColumns = 30;
         difficulty.maxBombs = 100;
+        difficulty.maxBullets = 10;
+        difficulty.maxShields = 5;
         break;
     default: // Multijugador
         break;
     }
-    game_menu();
 }
 
 // Función para generar coordenadas aleatorias de bombas
@@ -164,8 +182,8 @@ bool prove_coordinates(const vector<int> &coordinate, const vector<vector<int>> 
 {
 
     // Verifica si está fuera de rango
-    if ((coordinate[0] <= 0 || coordinate[0] > difficulty.maxRows) ||
-        (coordinate[1] <= 0 || coordinate[1] > difficulty.maxColumns))
+    if ((coordinate[0] <= 0 || coordinate[0] > difficulty.maxColumns) ||
+        (coordinate[1] <= 0 || coordinate[1] > difficulty.maxRows))
     {
         error_type.outOfRange = true;
         return true;
@@ -318,7 +336,7 @@ void load_game(vector<vector<int>> &bombXY)
     archivo.close();
     cout << "Partida cargada correctamente.\n";
 
-    game_multiplayer();
+    game_multiplayer(bombXY);
 }
 
 // Función para imprimir el tablero de juego
@@ -390,29 +408,22 @@ void print_board()
     cout << "Leyenda: # = Sin explorar, $ = Tesoro, ! = Bomba\n";
 }
 
-// Función principal de control del juego
-   int game_menu()
+int game_menu()
 {
     int opcion;
+    vector<vector<int>> bombXY;
 
     while (true)
     {
 
-        cout << R"(
-╔══════════════════════════════════════════════╗
-║         ███ ENCUENTRA EL TESORO ███          ║
-║──────────────────────────────────────────────║
-║  1. Jugar modo Solitario                     ║
-║  2. Jugar modo Multijugador                  ║
-║  3. Cargar partida                           ║
-║  4. Guardar partida                          ║
-║  5. Salir del juego                          ║
-╚══════════════════════════════════════════════╝
-Seleccione una opción: )";
+        cout <<Sprites.menu;
         cin >> opcion;
 
         if (opcion == 1)
         {
+            reset_game_state(bombXY);
+            game_data.max_players = 1;
+
             int dif;
             cout << "\nSelecciona dificultad:\n";
             cout << "1. Fácil (10x10 - 30 bombas)\n";
@@ -420,10 +431,16 @@ Seleccione una opción: )";
             cout << "3. Difícil (30x30 - 100 bombas)\n";
             cout << "Opción: ";
             cin >> dif;
+
             menu_difficulty(dif);
+            bombXY = random_coordinates();
+            player_configuration();
+            game_multiplayer(bombXY);
         }
         else if (opcion == 2)
         {
+            reset_game_state(bombXY);
+
             int dif;
             cout << "\nSelecciona dificultad para Multijugador:\n";
             cout << "1. Fácil (10x10 - 30 bombas)\n";
@@ -431,7 +448,6 @@ Seleccione una opción: )";
             cout << "3. Difícil (30x30 - 100 bombas)\n";
             cout << "Opción: ";
             cin >> dif;
-            menu_difficulty(dif);
 
             cout << "\n¿Con cuántos jugadores deseas jugar? (1-4): ";
             cin >> game_data.max_players;
@@ -442,24 +458,19 @@ Seleccione una opción: )";
                 continue;
             }
 
-            for (int i = 0; i < game_data.max_players; i++)
-            {
-                cout << "Nombre del jugador " << i + 1 << ": ";
-                cin >> players[i].name;
-                players[i].points = 0;
-                players[i].is_alive = true;
-            }
-
-            game_multiplayer();
+            menu_difficulty(dif);
+            player_configuration();
+            bombXY = random_coordinates();
+            game_multiplayer(bombXY);
         }
         else if (opcion == 3)
         {
-            load_game(game_data.bombXY);
+            load_game(bombXY);
             print_board();
         }
         else if (opcion == 4)
         {
-            save_game(game_data.bombXY);
+            save_game(bombXY);
         }
         else if (opcion == 5)
         {
@@ -475,9 +486,8 @@ Seleccione una opción: )";
     return 0;
 }
 
-
 // Función para modo multijugador
-int game_multiplayer()
+void game_multiplayer(vector<vector<int>> &bombXY)
 {
     bool lose = false;
     int positionX = 0, positionY = 0, retire = 0;
@@ -503,8 +513,10 @@ int game_multiplayer()
                 coordinate = {positionX, positionY};
 
                 // Verifica las coordenadas
-                lose = prove_coordinates(coordinate, game_data.bombXY);
+                lose = prove_coordinates(coordinate, bombXY);
                 game_data.repeat.push_back(coordinate);
+
+                player_action(turn, lose);
 
                 if (lose)
                 {
@@ -553,18 +565,95 @@ int game_multiplayer()
     {
         cout << players[i].name << ": " << players[i].points << " puntos\n";
     }
+}
 
-    return 0;
+void player_configuration()
+{
+    for (int i = 0; i < game_data.max_players; i++)
+    {
+        cout << "Nombre del jugador " << i + 1 << ": ";
+        cin >> players[i].name;
+        players[i].points = 0;
+        players[i].is_alive = true;
+        players[i].bullets = difficulty.maxBullets;
+        players[i].shields = difficulty.maxShields;
+    }
 }
 
 // Función para reiniciar el estado del juego
-void reset_game_state()
+void reset_game_state(vector<vector<int>> &bombXY)
 {
     game_data.reset();
     error_type.reset();
+    bombXY.clear();
     for (int i = 0; i < 4; i++)
     {
         players[i].points = 0;
         players[i].is_alive = false;
+    }
+}
+
+// Función para disparar con pistola
+void pistol_shot(int &turn, bool &lose)
+{
+    if (players[turn].bullets == 0)
+    {
+        cout << players[turn].name << " no tiene balas, no puede disparar\n";
+        return;
+    }
+    else if (lose)
+    {
+        cout << players[turn].name << " a hecho explotar una bomba! +10 puntos\n";
+    }
+    else
+    {
+        cout << players[turn].name << " a desperdiciado una bala!, no gana puntos\n";
+    }
+
+    players[turn].bullets--;
+    lose = false;
+}
+
+// Función para protegerse con escudo
+void shield_protection(int &turn, bool &lose)
+{
+    if (players[turn].shields == 0)
+    {
+        cout << players[turn].name << " no tiene escudos, no puede protegerse\n";
+        return;
+    }
+    else if (lose)
+    {
+        cout << players[turn].name << " se ha protegido de una bomba! +10 puntos\n";
+        players[turn].shields--;
+        lose = false;
+    }
+    else
+    {
+        cout << players[turn].name << " a gastado un escudo, no gana puntos\n";
+        players[turn].shields--;
+        lose = false;
+    }
+}
+
+void player_action(int &turn, bool &lose)
+{
+    int action;
+    cout << "¿Que desea hacer? (1=Excavar, 2=Protegerse, 3=Disparar con pistola): ";
+    cin >> action;
+    switch (action)
+    {
+    case 1: // Excavar
+        cout << "Usted ha excavado... \n";
+        break;
+    case 2: // Protegerse
+        shield_protection(turn, lose);
+        break;
+    case 3: // Disparar con pistola
+        pistol_shot(turn, lose);
+        break;
+    default:
+        cout << "Acción inválida, intente nuevamente\n";
+        break;
     }
 }
